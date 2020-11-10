5bcd01eea9b8b8be9ac09e1438c19d4fe9eedeb6
Janik Schumacher <rndmb0t@gmail.com>
2020-11-11 00:22:10 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,16 @@ import { SimpleLineLogger } from './simple-line-logger';
 const a1 = new SimpleLineLogger('');
 const a2 = new SimpleLineLogger('');
 const a3 = new SimpleLineLogger('');
-const p1 = new ProgressLogger({ totalProgress: 100, filledColor: 'GREEN' });
+const p1 = new ProgressLogger({ totalProgress: 100, filledColor: 'GREEN', maxWidth: 100 });
 const b = new MultilineLogger([a1, a2, a3, p1]);
 
-a1.setNextText('val1: ' + Math.random())
-a3.setNextText({ text: 'val3: ' + Math.random(), color: "CYAN" });
-
-a2.setNextText({ text: 'val2: ' + Math.random(), color: "MAGENTA" })
 
 
 setInterval(() => {
 
+  a1.setNextText('val1: ' + Math.random())
+  a3.setNextText({ text: 'val3: ' + Math.random(), color: "CYAN" });
+  a2.setNextText({ text: 'val2: ' + Math.random(), color: "MAGENTA" })
   p1.setProgress(Math.round(Math.random() * 100));
   b.render();
-}, 1000);
+}, 100);
