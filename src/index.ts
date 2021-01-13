cf8998f318921c1db89ce999b3acbb35ebaef5e0
Janik Schumacher <rndmb0t@gmail.com>
2021-01-13 22:39:34 +0100

    exporting everything

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,38 @@
 import { AwesomeLogger } from './awesome-logger';
 import { AwesomeChecklistLoggerState } from './logger/models/config/checklist';
 
-export { AwesomeLogger } from './awesome-logger';
+export * from './awesome-logger';
+export * from './types/logger-color';
+export * from './models/text-object';
+export * from './logger/types/logger-definitions';
+export * from './logger/types/logger-type';
+
+export * from './logger/models/logger-base';
+export * from './logger/models/checklist-logger';
+export * from './logger/models/multi-logger';
+export * from './logger/models/progress-logger';
+export * from './logger/models/spinner-logger';
+export * from './logger/models/text-logger';
+
+export * from './logger/models/config/checklist';
+export * from './logger/models/config/multi';
+export * from './logger/models/config/progress';
+export * from './logger/models/config/spinner';
+export * from './logger/models/config/text';
+
+export * from './interaction/types/prompt-definitions';
+export * from './interaction/types/prompt-type';
+
+export * from './interaction/models/prompt-base';
+export * from './interaction/models/text-prompt';
+export * from './interaction/models/toggle-prompt';
+
+export * from './interaction/models/config/base';
+export * from './interaction/models/config/text';
+export * from './interaction/models/config/toggle';
+
+export * from './utils/ansi-utils';
+
 
 // AwesomeLogger.log('text', { text: { text: 'awd', color: 'CYAN' } });
 // AwesomeLogger.logText({ text: 'awd', color: 'GREEN' });
@@ -62,23 +93,23 @@ export { AwesomeLogger } from './awesome-logger';
 //   console.log(x[0]);
 // });
 
-const checklistLogger = AwesomeLogger.log('checklist', {
-  items: [
-    {
-      state: AwesomeChecklistLoggerState.pending,
-      text: 'yeeeee1'
-    },
-    {
-      state: AwesomeChecklistLoggerState.pending,
-      text: 'yeeeee2'
-    },
-    {
-      state: AwesomeChecklistLoggerState.pending,
-      text: 'yeeeee3'
-    }
-  ]
-});
+// const checklistLogger = AwesomeLogger.log('checklist', {
+//   items: [
+//     {
+//       state: 'pending',
+//       text: 'yeeeee1'
+//     },
+//     {
+//       state: 'pending',
+//       text: 'yeeeee2'
+//     },
+//     {
+//       state: 'pending',
+//       text: 'yeeeee3'
+//     }
+//   ]
+// });
 
-setInterval(() => {
-  checklistLogger.changeState(Math.round(Math.random() * 2), Math.round(Math.random() * 6));
-}, 100);
+// setInterval(() => {
+//   checklistLogger.changeState(Math.round(Math.random() * 2), 'partiallySucceeded');
+// }, 100);
