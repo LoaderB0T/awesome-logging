0ee189f0665d4fdeaccd69c627c0b3e84d0d40b9
Janik Schumacher <rndmb0t@gmail.com>
2021-07-02 19:47:14 +0200

    changed string line printing

--- src/render/string-renderer.ts
+++ src/render/string-renderer.ts
@@ -63,41 +63,14 @@ export class StringRenderer {
       // We don't need to calculate anything if the strings are identical
       return '';
     }
-    const cleanOldString = stripAnsi(oldVal);
-    let cleanNewString = stripAnsi(newVal);
+    const oldStringLength = stripAnsi(oldVal).length;
+    let newStringLength = stripAnsi(newVal).length;
 
-    const cleanStringsEqual = cleanOldString === cleanNewString;
-    if (cleanStringsEqual) {
-      // The strings only differ in special ansi modifiers but the text is the same
-      // Detecting what changed in terms of ansi stuff is very difficult to detect performance wise
-      // Rewriting the whole string takes less time in that case
-      return newVal;
-    }
-    const oldStrLen = cleanOldString.length;
-    let newStrLen = cleanNewString.length;
-    if (newStrLen > oldStrLen) {
-      // When the new string is longer than the old string, it will just overwrite anything
-      return newVal;
-    }
-    let newStringWouldHaveBeenShorter = false;
-    while (oldStrLen > newStrLen) {
+    while (oldStringLength > newStringLength) {
       // If the new string is shorter than the old string, we need to "erase" the difference by printing spaces
       newVal += ' ';
-      cleanNewString += ' ';
-      newStrLen++;
-      newStringWouldHaveBeenShorter = true;
+      newStringLength++;
     }
-    if (newStringWouldHaveBeenShorter) {
-      // We know that we have to print everything anyways because of the erasing spaces at the end, no need to calculate the difference
-      return newVal;
-    }
-    for (let i = newStrLen - 1; i >= 0; i--) {
-      const oldCharAtI = cleanOldString.substr(i, 1);
-      const newCharAtI = cleanNewString.substr(i, 1);
-      if (oldCharAtI !== newCharAtI) {
-        return sliceAnsi(newVal, 0, i + 1);
-      }
-    }
-    return '';
+    return newVal;
   }
 }
