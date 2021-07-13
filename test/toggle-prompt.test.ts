f2eda34e3144d73e454233e7f1ecea7d6d7a5606
Janik Schumacher <rndmb0t@gmail.com>
2021-07-13 20:25:38 +0200

    added basic toggle prompt tests

---
+++ test/toggle-prompt.test.ts
@@ -0,0 +1,128 @@
+import { Terminal } from 'node-terminal-emulator';
+import { AwesomeLogger } from '../src/index';
+import { Stdout } from '../src/render/stdout';
+import { TestInit } from './reset';
+
+describe('Toggle Prompt', () => {
+  let t: Terminal;
+
+  beforeEach(() => {
+    t = TestInit.getTerminal();
+  });
+
+  afterEach(() => {
+    t.restoreStdout(Stdout.getInstance());
+  });
+
+  test('three options', done => {
+    const c = AwesomeLogger.prompt('toggle', { options: ['option 1', 'option 2', 'option 3'] });
+    c.result.then(r => {
+      expect(r).toStrictEqual(['option 1', 'option 2']);
+      expect(t.text).toStrictEqual(['', ' - Selected options: option 1, option 2']);
+      done();
+    });
+    expect(t.text).toStrictEqual(['', '[ ] option 1', '[ ] option 2', '[ ] option 3']);
+    expect(c.getCurrentAnswer()).toStrictEqual([]);
+    t.sendKey('down');
+    expect(t.text).toStrictEqual(['', '[ ] option 1', '[ ] option 2', '[ ] option 3']);
+    expect(c.getCurrentAnswer()).toStrictEqual([]);
+    t.sendText(' ');
+    expect(t.text).toStrictEqual(['', '[ ] option 1', '[X] option 2', '[ ] option 3']);
+    expect(c.getCurrentAnswer()).toStrictEqual(['option 2']);
+    t.sendKey('down');
+    t.sendText(' ');
+    expect(t.text).toStrictEqual(['', '[ ] option 1', '[X] option 2', '[X] option 3']);
+    expect(c.getCurrentAnswer()).toStrictEqual(['option 2', 'option 3']);
+    t.sendText(' ');
+    t.sendKey('up');
+    t.sendKey('up');
+    t.sendText(' ');
+    t.sendKey('enter');
+  });
+
+  test('10 options', done => {
+    const c = AwesomeLogger.prompt('toggle', {
+      options: [
+        'option 1',
+        'option 2',
+        'option 3',
+        'option 4',
+        'option 5',
+        'option 6',
+        'option 7',
+        'option 8',
+        'option 9',
+        'option 10'
+      ]
+    });
+    c.result.then(r => {
+      expect(r).toStrictEqual(['option 3', 'option 7']);
+      expect(t.text).toStrictEqual(['', ' - Selected options: option 3, option 7']);
+      done();
+    });
+    expect(t.text).toStrictEqual([
+      '',
+      '| [ ] option 1',
+      '| [ ] option 2',
+      '| [ ] option 3',
+      '| [ ] option 4',
+      '| [ ] option 5',
+      '↓ [ ] option 6'
+    ]);
+    expect(c.getCurrentAnswer()).toStrictEqual([]);
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendText(' ');
+    expect(t.text).toStrictEqual([
+      '',
+      '| [ ] option 1',
+      '| [ ] option 2',
+      '| [X] option 3',
+      '| [ ] option 4',
+      '| [ ] option 5',
+      '↓ [ ] option 6'
+    ]);
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendText(' ');
+    expect(t.text).toStrictEqual([
+      '',
+      '↑ [X] option 3',
+      '| [ ] option 4',
+      '| [ ] option 5',
+      '| [ ] option 6',
+      '| [X] option 7',
+      '↓ [ ] option 8'
+    ]);
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendKey('down');
+    expect(t.text).toStrictEqual([
+      '',
+      '↑ [ ] option 5',
+      '| [ ] option 6',
+      '| [X] option 7',
+      '| [ ] option 8',
+      '| [ ] option 9',
+      '| [ ] option 10'
+    ]);
+
+    // Edge case of pressing down on the last item
+    t.sendKey('down');
+    t.sendKey('down');
+    t.sendKey('down');
+    expect(t.text).toStrictEqual([
+      '',
+      '↑ [ ] option 5',
+      '| [ ] option 6',
+      '| [X] option 7',
+      '| [ ] option 8',
+      '| [ ] option 9',
+      '| [ ] option 10'
+    ]);
+
+    t.sendKey('enter');
+  });
+});
