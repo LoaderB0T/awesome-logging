cecbfef0aae2df0c4134d5f1bdece7414f0c7db3
Janik Schumacher <rndmb0t@gmail.com>
2022-03-31 23:50:33 +0200

    marked known useless expression, because it is actually not quite useless

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ export * from './logger';
 export * from './prompt';
 
 // This is a workaround for initializing the static AwesomeLogger before everything else
-AwesomeLogger.restrictedLogging;
+AwesomeLogger.restrictedLogging; // lgtm [js/useless-expression]
 
 LoggerManager.getInstance();
