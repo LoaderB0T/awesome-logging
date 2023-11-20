4a2506a846adcf3cb64a4ff2540459a775be41e4
Janik Schumacher <janik.schumacher@helpline.pmcs.de>
2023-11-20 16:05:12 +0100

    Path fixes

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,20 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
+import { defaultsESM } from 'ts-jest/presets';
+
+// Changes the tsconfig used for tests to tsconfig.spec.json
+const transform = defaultsESM.transform!;
+Object.keys(transform).forEach(key => {
+  const value = transform[key];
+  if (Array.isArray(value)) {
+    value[1].tsconfig = './tsconfig.spec.json';
+  }
+});
 
 const config: JestConfigWithTsJest = {
   roots: ['./test'],
-  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    ...transform,
+  },
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
