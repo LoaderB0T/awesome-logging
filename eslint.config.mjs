d7a6a78f745be2b383b2e57a13d95f0e4e33592c
Janik Schumacher <rndmb0t@gmail.com>
2024-10-04 01:16:33 +0200

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,6 @@ export default [
   {
     ignores: ['test/**/*', '**/jest.config.ts', 'eslint.config.mjs'],
   },
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.js', '*.mjs'],
-          defaultProject: true,
-        },
-        tsconfigRootDir: import.meta.name,
-        allowDefaultProject: true,
-      },
-    },
-  },
   ...awdwareNode,
   {
     rules: {
